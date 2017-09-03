39373b1ae7dba00fe7dec1476e5319aeccc769d0
SimoneBronzini <simone.bronzini@gmail.com>
2017-09-03 19:24:36 +0200

    add copyright to all files

--- btcpy/lib/parsing.py
+++ btcpy/lib/parsing.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2017 chainside srl
+#
+# This file is part of the btcpy package.
+#
+# It is subject to the license terms in the LICENSE.md file found in the top-level
+# directory of this distribution.
+#
+# No part of btcpy, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE.md file.
+
 from binascii import hexlify, unhexlify
 from hashlib import sha256
 import hashlib
---
+++ btcpy/structs/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2017 chainside srl
+#
+# This file is part of the btcpy package.
+#
+# It is subject to the license terms in the LICENSE.md file found in the top-level
+# directory of this distribution.
+#
+# No part of btcpy, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE.md file.
+
