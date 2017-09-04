8071e0dd971a3c7b7ecb9c6d630392d16f5eca30
SimoneBronzini <simone.bronzini@gmail.com>
2017-09-05 01:02:29 +0200

    add test vectors for signatures

--- tests/unit.py
+++ tests/unit.py
@@ -1773,7 +1773,7 @@ class TestStandardness(unittest.TestCase):
         pass
 
 
-class TestSegwitDigest(unittest.TestCase):
+class TestSegwitSigs(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -1791,7 +1791,23 @@ class TestSegwitDigest(unittest.TestCase):
                                        '1d0f172a0ecb48aee1be1f2687d2963ae33f71a188ac0046c32300000000ffffffff863e'
                                        'f3e1a92afbfdb97f31ad0fc7683ee943e9abcf2501590ff8f6551f47e5e5110000000100'
                                        '0000',
-                      'sighash': 'c37af31116d1b27caf68aae9e3ac82f1477929014d5b917657d0eb49478cb670'},
+                      'sighash': 'c37af31116d1b27caf68aae9e3ac82f1477929014d5b917657d0eb49478cb670',
+                      'signature': '304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a02205'
+                                   '73a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee',
+                      'txins': [{'prev_script': '2103c9f4836b9a4f77fc0d81f7bcb01b7f1b35916864b9476c241ce9fc198bd25432ac',
+                                 'prev_amount': 625000000},
+                                {'prev_script': '00141d0f172a0ecb48aee1be1f2687d2963ae33f71a1',
+                                 'prev_amount': 600000000,
+                                 'digest_preimage': '0100000096b827c8483d4e9b96712b6713a7b68d6e8003a781feba36c31'
+                                                    '143470b4efd3752b0a642eea2fb7ae638c36f6252b6750293dbe574a806'
+                                                    '984b8e4d8548339a3bef51e1b804cc89d182d279655c3aa89e815b1b309'
+                                                    'fe287d9b2b55d57b90ec68a010000001976a9141d0f172a0ecb48aee1be'
+                                                    '1f2687d2963ae33f71a188ac0046c32300000000ffffffff863ef3e1a92'
+                                                    'afbfdb97f31ad0fc7683ee943e9abcf2501590ff8f6551f47e5e5110000'
+                                                    '0001000000',
+                                 'digest': 'c37af31116d1b27caf68aae9e3ac82f1477929014d5b917657d0eb49478cb670',
+                                 'privk': PrivateKey.unhexlify('619c335025c7f4012e556c2a58b2506e30b8511b53ade95e'
+                                                               'a316fd8c3286feb9')}]},
                      {'unsigned_tx': '0100000001db6b1b20aa0fd7b23880be2ecbd4a98130974cf4748fb66092ac4d3ceb1a5477'
                                      '0100000000feffffff02b8b4eb0b000000001976a914a457b684d7f0d539a46a45bbc043f3'
                                      '5b59d0d96388ac0008af2f000000001976a914fd270b1ee6abcaea97fea7ad0402e8bd8ad6'
@@ -1805,22 +1821,21 @@ class TestSegwitDigest(unittest.TestCase):
                                        '79091972186c449eb1ded22b78e40d009bdf008988ac00ca9a3b00000000feffffffde98'
                                        '4f44532e2173ca0d64314fcefe6d30da6f8cf27bafa706da61df8a226c83920400000100'
                                        '0000',
-                      'sighash': '64f3b0f4dd2bb3aa1ce8566d220cc74dda9df97d8490cc81d89d735c92e59fb6'},
-                     # {'unsigned_tx': '0100000002fe3dc9208094f3ffd12645477b3dc56f60ec4fa8e6f5d67c565d1c6b9216b36e'
-                     #                 '0000000000ffffffff0815cf020f013ed6cf91d29f4202e8a58726b1ac6c79da47c23d1bee'
-                     #                 '0a6925f80000000000ffffffff0100f2052a010000001976a914a30741f8145e5acadf23f7'
-                     #                 '51864167f32e0963f788ac00000000',
-                     #  'hash_prevouts': 'ef546acf4a020de3898d1b8956176bb507e6211b5ed3619cd08b6ea7e2a09d41',
-                     #  'hash_sequence': '',
-                     #  'hash_outputs': 'de984f44532e2173ca0d64314fcefe6d30da6f8cf27bafa706da61df8a226c83',
-                     #  'hash_preimage': '01000000b0287b4a252ac05af83d2dcef00ba313af78a3e9c329afa216eb3aa2a7b4613a'
-                     #                   '18606b350cd8bf565266bc352f0caddcf01e8fa789dd8a15386327cf8cabe198db6b1b20'
-                     #                   'aa0fd7b23880be2ecbd4a98130974cf4748fb66092ac4d3ceb1a5477010000001976a914'
-                     #                   '79091972186c449eb1ded22b78e40d009bdf008988ac00ca9a3b00000000feffffffde98'
-                     #                   '4f44532e2173ca0d64314fcefe6d30da6f8cf27bafa706da61df8a226c83920400000100'
-                     #                   '0000',
-                     #  'sighash': '64f3b0f4dd2bb3aa1ce8566d220cc74dda9df97d8490cc81d89d735c92e59fb6'},
-
+                      'sighash': '64f3b0f4dd2bb3aa1ce8566d220cc74dda9df97d8490cc81d89d735c92e59fb6',
+                      'signature': '3044022047ac8e878352d3ebbde1c94ce3a10d057c24175747116f8288e5d794d12d482f0220'
+                                   '217f36a485cae903c713331d877c1f64677e3622ad4010726870540656fe9dcb',
+                      'txins': [{'prev_script': '001479091972186c449eb1ded22b78e40d009bdf0089',
+                                 'prev_amount': 1000000000,
+                                 'digest_preimage': '01000000b0287b4a252ac05af83d2dcef00ba313af78a3e9c329afa216e'
+                                                    'b3aa2a7b4613a18606b350cd8bf565266bc352f0caddcf01e8fa789dd8a'
+                                                    '15386327cf8cabe198db6b1b20aa0fd7b23880be2ecbd4a98130974cf47'
+                                                    '48fb66092ac4d3ceb1a5477010000001976a91479091972186c449eb1de'
+                                                    'd22b78e40d009bdf008988ac00ca9a3b00000000feffffffde984f44532'
+                                                    'e2173ca0d64314fcefe6d30da6f8cf27bafa706da61df8a226c83920400'
+                                                    '0001000000',
+                                 'digest': '64f3b0f4dd2bb3aa1ce8566d220cc74dda9df97d8490cc81d89d735c92e59fb6',
+                                 'privk': PrivateKey.unhexlify('eb696a065ef48a2192da5b28b694f87544b30fae8327c451'
+                                                               '0137a922f32c6dcf')}]},
                 ]
 
     def test_hash_prevouts(self):
@@ -1837,40 +1852,35 @@ class TestSegwitDigest(unittest.TestCase):
         for tx in self.data:
             unsigned = SegWitTransaction.unhexlify(tx['unsigned_tx'])
             self.assertEqual(unsigned._hash_outputs(), bytearray(unhexlify(tx['hash_outputs'])))
+            
+    def test_digest_preimage(self):
+        for tx in self.data:
+            unsigned = SegWitTransaction.unhexlify(tx['unsigned_tx'])
+            for j, txin in enumerate(tx['txins']):
+                if 'digest_preimage' in txin:
+                    self.assertEqual(unsigned._get_segwit_digest_preimage(j,
+                                                                          ScriptBuilder.identify(txin['prev_script']),
+                                                                          txin['prev_amount']).hexlify(),
+                                     txin['digest_preimage'])
 
-    # def test_sighash(self):
-    #     cache = {'hash_prevouts': None,
-    #              'hash_sequence': None,
-    #              'hash_outputs': None}
-    #     spend = SpendingData()
-    #     for tx in self.data:
-    #         unsigned = SegWitTransaction.unhexlify(tx['unsigned_tx'])
-    #         self.assertEqual(unsigned.get_digest_for_input(0), bytearray(unhexlify(tx['hash_outputs'])))
+    def test_digest(self):
+        for tx in self.data:
+            unsigned = SegWitTransaction.unhexlify(tx['unsigned_tx'])
+            for j, txin in enumerate(tx['txins']):
+                if 'digest' in txin:
+                    self.assertEqual(hexlify(unsigned.get_segwit_digest(j,
+                                                                        ScriptBuilder.identify(txin['prev_script']),
+                                                                        txin['prev_amount'])).decode(),
+                                     txin['digest'])
 
-    # def test_p2wpkh(self):
-    #     unsigned = '0100000002fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000000eeffffffef51e'
-    #                '1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb2060000000019'
-    #                '76a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d'
-    #                '50ce2f0167faa815988ac11000000'
-    #     prev1 = bytearray(unhexlify('2103c9f4836b9a4f77fc0d81f7bcb01b7f1b35916864b9476c241ce9fc198bd25432ac'))
-    #     priv1 = PrivateKey.unhexlify('bbc27228ddcb9209d7fd6f36b02f7dfa6252af40bb2f1cbc7a557da8027ff866')
-    #     prev2 = bytearray(unhexlify('00141d0f172a0ecb48aee1be1f2687d2963ae33f71a1'))
-    #     priv2 = PrivateKey.unhexlify('619c335025c7f4012e556c2a58b2506e30b8511b53ade95ea316fd8c3286feb9')
-    #     spend = [SpendingData(ScriptBuilder.identify(prev1), priv1, prev_amount=625000000),
-    #              SpendingData(ScriptBuilder.identify(prev2), priv2, prev_amount=600000000)]
-    #     spending = SegWitTransaction.unhexlify(unsigned).to_mutable()
-    #     spending.sign(spend)
-    #     expected = '01000000000102fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000049483045022'
-    #                '1008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0'
-    #                'a19c0489bc22ede944ccf4ecbab4cc618ef3ed01eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b'
-    #                '2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d'
-    #                '5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac000247304402203609e17b8'
-    #                '4f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f519'
-    #                '28d43c212a8caed02de67eebee0121025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee63571'
-    #                '1000000'
-    #     print(spending.hexlify())
-    #     print(expected)
-    #     self.assertTrue(spending.hexlify() == expected)
+    def test_sig(self):
+        for tx in self.data:
+            if 'signature' in tx:
+                for txin in tx['txins']:
+                    if 'privk' in txin and 'digest' in txin:
+                        self.assertEqual(hexlify(txin['privk'].sign(bytearray(unhexlify(txin['digest'])),
+                                                                    deterministic=True)).decode(),
+                                         tx['signature'])
 
 
 if __name__ == '__main__':
