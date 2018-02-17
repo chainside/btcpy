b8afd1e45b4faab3e7a567291d05d4e4153dc9a3
Peerchemist <dx.1178@gmail.com>
2018-02-17 19:14:37 +0100

    expand the unittest

--- tests/base58test.py
+++ tests/base58test.py
@@ -1,6 +1,24 @@
 import unittest
+from binascii import hexlify, unhexlify
 from btcpy.lib.base58 import b58decode, b58encode
 from btcpy.lib.base58 import b58decode_check, b58encode_check
+
+
+# following test cases come from: https://github.com/bitcoin/bitcoin/blob/master/src/test/data/base58_encode_decode.json
+test_strings = [
+            ["", ""],
+            ["61", "2g"],
+            ["626262", "a3gV"],
+            ["636363", "aPEr"],
+            ["73696d706c792061206c6f6e6720737472696e67", "2cFupjhnEsSn59qHXstmK2ffpLv2"],
+            ["00eb15231dfceb60925886b67d065299925915aeb172c06647", "1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L"],
+            ["516b6fcd0f", "ABnLTmg"],
+            ["bf4f89001e670274dd", "3SEo3LWLoPntC"],
+            ["572e4794", "3EFU7m"],
+            ["ecac89cad93923c02321", "EJDM8drfXA6uyA"],
+            ["10c8511e", "Rt5zm"],
+            ["00000000000000000000", "1111111111"]
+    ]
 
 
 class B58Test(unittest.TestCase):
@@ -11,7 +29,8 @@ class B58Test(unittest.TestCase):
 
     def test_b58encode(self):
 
-        self.assertEqual(b58encode("hello".encode()), 'Cn8eVZg')
+        for i in test_strings:
+            self.assertEqual(b58encode(unhexlify(i[0])), i[1])
 
     def test_b58encode_check(self):
 
@@ -19,7 +38,9 @@ class B58Test(unittest.TestCase):
 
     def test_b58decode(self):
 
-        self.assertEqual(b58decode('Cn8eVZg'), b'hello')
+        for i in test_strings:
+            i = i[::-1]  # flip it
+            self.assertEqual(hexlify(b58decode(i[0])).decode(), i[1])
 
     def test_b58decode_check(self):
 
