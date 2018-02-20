638340c43dd448e03332569f904818864a38067d
SimoneBronzini <simone.bronzini@gmail.com>
2018-02-20 14:25:17 +0100

    version 0.3.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='chainside-btcpy',
-      version='0.3.0',
+      version='0.3.1',
       packages=find_packages(),
       install_requires=['ecdsa==0.13'],
       extras_require={'develop': ['python-bitcoinlib==0.7.0']},
@@ -22,6 +22,6 @@ setup(name='chainside-btcpy',
       author='chainside srl',
       author_email='simone.bronzini@chainside.net',
       url='https://github.com/chainside/btcpy',
-      download_url='https://github.com/chainside/btcpy/archive/0.3.0.tar.gz',
+      download_url='https://github.com/chainside/btcpy/archive/0.3.1.tar.gz',
       python_requires='>=3',
       keywords=['bitcoin', 'blockchain', 'bitcoind', 'chainside'])
