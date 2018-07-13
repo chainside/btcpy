3c21177f0e3b04a8f1fbc48a0a481761743aa7e0
SimoneBronzini <simone.bronzini@gmail.com>
2018-07-13 18:30:22 +0200

    adapt tests to new solvers

--- tests/integration.py
+++ tests/integration.py
@@ -314,12 +314,12 @@ class TestSpends(unittest.TestCase):
                         elif embedder.get_name() == 'relativetime':
                             emb = embedder(scripts=[script['script']])
                             embedded.append((emb.instance,
-                                             (TimelockSolver(script['solver']), emb.instance),
+                                             (RelativeTimelockSolver(Sequence(3), script['solver']), emb.instance),
                                              '{}({})'.format(embedder.get_name(), script['name'])))
                         elif embedder.get_name() == 'absolutetime':
                             emb = embedder(scripts=[script['script']])
                             embedded.append((emb.instance,
-                                             (TimelockSolver(script['solver']), emb.instance),
+                                             (AbsoluteTimelockSolver(Locktime(100), script['solver']), emb.instance),
                                              '{}({})'.format(embedder.get_name(), script['name'])))
                         else:
                             raise ValueError('Unknown embedder: {}'.format(embedder.get_name()))
@@ -446,8 +446,6 @@ class TestSpends(unittest.TestCase):
         regtest.send_rpc_cmd(['generate', '100'], 0)
 
         generate = False
-        next_locktime = Locktime(0)
-        next_sequence = Sequence.max()
 
         i = 0
         while i < len(self.all) - 2:
@@ -460,7 +458,7 @@ class TestSpends(unittest.TestCase):
                 outs.append(TxOut(unspent['txout'].value - 1000000, j, script[0]))
                 prev_types.append(script[2])
 
-            tx = MutableTransaction(2, ins, outs, min_locktime(unspent['next_locktime'] for unspent in utxo))
+            tx = MutableTransaction(2, ins, outs, Locktime(0))
             mutable = copy.deepcopy(tx)
             tx = tx.spend([unspent['txout'] for unspent in utxo], [unspent['solver'] for unspent in utxo])
 
@@ -471,10 +469,10 @@ class TestSpends(unittest.TestCase):
             # print()
             # print('raw: {}'.format(tx.hexlify()))
             # print('prev_scripts, amounts, solvers:')
+            # print('TX: {}'.format(i))
 
-            print('TX: {}'.format(i))
             regtest.send_rpc_cmd(['sendrawtransaction', tx.hexlify()], 0)
-            print('Mempool size: {}'.format(len(regtest.send_rpc_cmd(['getrawmempool'], 0))))
+            # print('Mempool size: {}'.format(len(regtest.send_rpc_cmd(['getrawmempool'], 0))))
 
             if cmdline_args.dumpfile is not None:
                 with open(cmdline_args.dumpfile, 'a') as out:
@@ -486,28 +484,20 @@ class TestSpends(unittest.TestCase):
 
             for j, (output, prev_type) in enumerate(zip(tx.outs, prev_types)):
 
-                if 'time' in prev_type:
-                    if 'absolute' in prev_type:
-                        next_locktime = Locktime(100)
-                        next_sequence = Sequence(0xfffffffe)
-                    if 'relative' in prev_type:
-                        next_sequence = Sequence(3)
-                        generate = True
-                else:
-                    next_locktime = Locktime(0)
-                    next_sequence = Sequence.max()
+                if 'relativetime' in prev_type:
+                    generate = True
 
                 utxo.append({'txid': tx.txid,
                              'txout': output,
                              'solver': self.all[i+j][1][0],  # solver
-                             'next_seq': next_sequence,
-                             'next_locktime': next_locktime})
+                             'next_seq': Sequence.max(),
+                             'next_locktime': Locktime(0)})
             if generate:
                 regtest.send_rpc_cmd(['generate', '4'], 0)
                 generate = False
 
             if not i % 10:
-                print('generating 2')
+                # print('generating 2')
                 regtest.send_rpc_cmd(['generate', '2'], 0)
 
             i += 1
